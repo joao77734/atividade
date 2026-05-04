2495b2c7a31bd964788dd4b1da4056cdf8ac6f2b
joao77734 <oshymist@gmail.com>
2026-05-04 16:10:03 -0400

    atividade avaliativa ederson dia 30/04

--- Atividade Avaliativa Victor Alexandre e João Paulo.py
+++ Atividade Avaliativa Victor Alexandre e João Paulo.py
@@ -1,42 +1,179 @@
-lista_produto = [["Maça",19.90,"0123"],["Banana",2.90,"1234"],["Pera",8.90,"123234"]]
+nome_gerente = "1234"
+senha_gerente = "1234"
+nome_lista = []
+endereco_lista = []
+telefone_lista = []
+cpf_lista = []
+agencia_lista = []
+conta_lista = []
+saldo_lista = []
+email_lista = []
+nascimento_lista = []
+tipo_lista = []
+senha_lista = []
+limite_lista = []
 
-atualizar_produto = True
+print ("===== Login =====")
+print ("Informe as informações abaixo:")
+nome_login = str (input ("Usuário: "))
+senha_login = str (input ("Senha: "))
 
-#nome_do_produto ; valor_do_produto ; codigo_do_produto
-
-if atualizar_produto == True:
-
-
-    while atualizar_produto == True:
-
-        print("====================================================================")
-        print("                     ATUALIZANDO PRODUTOS                           ")
-        print("====================================================================")
-        print("Quantidade de produtos: ")
-        print(len(lista_produto))
-        print("====================================================================\n")
-
-        for i, produto in enumerate(lista_produto):
-            print(f"{i} - Nome: {produto[0]}, Preço:R$ {produto[1]},Código:{produto[2]}")
-
-        indice = int(input("Digite o número do produto que deseja atualizar: "))
-
-        print("1- Nome")
-        print("2- Preço")
-        print("1- Código")
-
-        opcao = input("O que deseja atualizar?")
+if nome_login == nome_gerente and senha_login == senha_gerente:
+    print ("\nBem-Vindo!\n")
+    while True:
+        print ("===== Menu =====")
+        print ("1 - Cadastro de Cliente")
+        print ("2 - Consulta de Cadastro")
+        print ("3 - Exclusão de Cadastro")
+        print ("4 - Ver Saldo")
+        print ("5 - Depósito")
+        print ("6 - Saque")
+        print ("7- Sair")
+        opcao = str (input ("Digte a opção desejada: "))
 
         if opcao == "1":
-            novo_nome = input("Novo Nome:")
-            lista_produto[indice][0] = novo_nome
+    
+            print ("\nDigite as informações pedidas abaixo:\n")
+            nome = str (input ("Nome: "))
+            endereco = str (input ("Endereço: "))
+            telefone = str(input("Telefone: "))
+            cpf = str(input("CPF: "))
+            agencia = str(input("Agência: "))
+            conta = str(input("Número da Conta: "))
+            saldo = float (input("Saldo: "))
+            email = str(input("Email: "))
+            nascimento = str(input("Data de Nascimento: "))
+            tipo = str(input("Tipo de Conta: "))
+            senha = str(input("Senha: "))
+            limite = str(input("Limite de Crédito: "))
+
+            nome_lista.append (nome)
+            endereco_lista.append (endereco)
+            telefone_lista.append (telefone)
+            cpf_lista.append (cpf)
+            agencia_lista.append (agencia)
+            conta_lista.append (conta)
+            saldo_lista.append (saldo)
+            email_lista.append (email)
+            nascimento_lista.append (nascimento)
+            tipo_lista.append (tipo)
+            senha_lista.append (senha)
+            limite_lista.append (limite)
+
+            print ("\nCliente Cadastrado!\n")
+        
         elif opcao == "2":
-            novo_preco = float(input("Novo Preço:"))
-            lista_produto[indice][1] = novo_preco
+            cpf_consulta = str (input ("\nDigite o CPF do cliente desejado: "))
+
+            if cpf_consulta in cpf_lista:
+                indice = cpf_lista.index (cpf_consulta)
+
+                print ("\nNome: ", nome_lista [indice])
+                print ("Endereço: ", endereco_lista [indice])
+                print ("Telefone: ", telefone_lista [indice])
+                print ("CPF: ", cpf_lista [indice])
+                print ("Agência: ", agencia_lista [indice])
+                print ("Conta: ", conta_lista [indice])
+                print ("Saldo: ", saldo_lista [indice])
+                print ("E-mail: ",email_lista [indice])
+                print ("Data de Nascimento: ", nascimento_lista [indice])
+                print ("Tipo de Conta: ", tipo_lista [indice])
+                print ("Limite de Crédito: ", limite_lista [indice], "\n")
+
+            else:
+                print ("\nCPF não Encontrado!\n")
+
         elif opcao == "3":
-            novo_codigo = input("Novo Código:")
-            lista_produto[indice][2] = novo_codigo
+            cpf_excluir = str (input ("\nDigite o CPF do cliente desejado: "))
+            
+            if cpf_excluir in cpf_lista:
+                indice = cpf_lista.index (cpf_excluir)
+
+                nome_lista.pop (indice)
+                endereco_lista.pop (indice)
+                telefone_lista.pop (indice)
+                cpf_lista.pop (indice)
+                agencia_lista.pop (indice)
+                conta_lista.pop (indice)
+                saldo_lista.pop (indice)
+                email_lista.pop (indice)
+                nascimento_lista.pop (indice)
+                tipo_lista.pop (indice)
+                senha_lista.pop (indice)
+                limite_lista.pop (indice)
+
+                print ("\nDados excluídos com sucesso!\n")
+
+            else:
+                print ("\nCPF não Encontrado!\n")
+        
+        elif opcao == "4":
+            conta_saldo = str (input ("\nDigite o número da conta desejada: "))
+
+            if conta_saldo in conta_lista:
+                indice = conta_lista.index (conta_saldo)
+
+                senha_saldo = str (input ("\nDigite a senha: "))
+
+                if senha_saldo in senha_lista [indice]:
+                    print ("Saldo da Conta: %.2f" %saldo_lista [indice])
+
+                else:
+                    print ("\nSenha Incorreta!\n")
+
+            else:
+                print ("\nConta não encontrada!\n")
+
+        elif opcao == "5":
+            conta_deposito = str (input ("\nDigite o número da conta desejada: "))
+
+            if conta_deposito in conta_lista:
+                indice = conta_lista.index (conta_deposito)
+
+                deposito = float (input ("Digite o valor que deseja depositar: "))
+
+                saldo_deposito = saldo_lista [indice] + deposito
+
+                saldo_lista [indice] = saldo_deposito
+            
+            else:
+                print ("\nConta não encontrada!\n")
+        
+        elif opcao == "6":
+            conta_saque = str (input ("\nDigite o número da conta desejada: "))
+
+            if conta_saque in conta_lista:
+                indice = conta_lista.index (conta_saque)
+
+                senha_saque = str (input ("Digite a senha: "))
+
+                if senha_saque == senha_lista [indice]:
+
+                    saque = float (input ("Digite o valor que deseja sacar: "))
+
+                    if saque < saldo_lista [indice]:
+
+                        saldo_saque = saldo_lista [indice] - saque
+
+                        saldo_lista [indice] = saldo_saque
+
+                        print ("\nSaque Realizado com Sucesso!\n")
+
+                    else:
+                        print ("\nSaldo Insuficiente!\n")
+                    
+                else:
+                    print ("\nSenha Incorreta!\n")
+
+            else:
+                print ("\nConta não encontrada!\n")
+            
+        elif opcao == "7":
+            print ("\nSaindo...\n")
+            break
 
         else:
-            print("Opção inválida")
-        print("Produto Atualizado!")
+            print ("\nOpção Inválida!\n")
+    
+else:
+    print ("\nUsuário/Senha Incorretos!\n")
